44b93e59fbe2e801d5ee792dfab34f7d75898a6d
AzureAD\EwanLaing <ewan.laing@2itesting.com>
2024-07-28 18:46:16 +0100

    2nd commit

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.use('/users', userRoutes);
 
 app.get('/', (req, res) => res.send('HELLO FROM HOMEPAGE'));
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port: http://localhost:${PORT}`));
